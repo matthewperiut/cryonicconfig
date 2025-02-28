373d815f4b6cda03ecb5b602cd296c48ef706c39
matthewperiut <matthewperiut@gmail.com>
2025-02-28 08:47:42 -0500

    1.20.4

---
+++ forge/src/main/java/com/periut/cryonicconfig/forge/CryonicConfigForge.java
@@ -0,0 +1,12 @@
+package com.periut.cryonicconfig.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+import com.periut.cryonicconfig.UtilityCryonicConfig;
+
+@Mod(UtilityCryonicConfig.MOD_ID)
+public final class CryonicConfigForge {
+    public CryonicConfigForge() {
+
+    }
+}
--- neoforge/src/main/java/com/periut/cryonicconfig/neoforge/CryonicConfigNeoForge.java
+++ neoforge/src/main/java/com/periut/cryonicconfig/neoforge/CryonicConfigNeoForge.java
@@ -1,8 +1,7 @@
 package com.periut.cryonicconfig.neoforge;
 
-import net.neoforged.fml.common.Mod;
-
 import com.periut.cryonicconfig.UtilityCryonicConfig;
+import net.neoforged.fml.common.Mod;
 
 @Mod(UtilityCryonicConfig.MOD_ID)
 public final class CryonicConfigNeoForge {
